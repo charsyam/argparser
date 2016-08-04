4ec170147669a3ad9b8b6535f94e08e5ab1a9037
charsyam <charsyam@naver.com>
2016-08-04 20:44:58 +0900

    refactoring argparser to be simple

--- argparser.h
+++ argparser.h
@@ -22,14 +22,8 @@ public:
     ArgParser(int argc, char **argv) : argc_(argc), argv_(argv) {
     }
 
-    std::map<int, struct Command> *getArgs(Type type) {
-        if (type == STRING) {
-            return &string_args_;
-        } else if (type == BOOLEAN) {
-            return &boolean_args_;
-        } 
-
-        return &exists_args_;
+    std::map<int, struct Command> *getArgs() {
+        return &args_;
     }
 
     int getNeedParam(Type type) {
@@ -50,7 +44,7 @@ public:
         command.type = type;
         command.description = desc; 
         
-        std::map<int, struct Command> *args = getArgs(type);
+        std::map<int, struct Command> *args = getArgs();
         args->insert(std::pair<int, struct Command>(opt, command));
         return 1;
     }
@@ -63,53 +57,36 @@ public:
         return 0;
     }
 
-    int find(int option, std::map<int, struct Command> *args) {
-        if (args->find(option) == args->end()) {
-            return 0;
+    struct Command *find(int option, std::map<int, struct Command> *args) {
+        auto iter = args->find(option);
+        if (iter == args->end()) {
+            return nullptr;
         }
 
-        return 1;
-    }
-
-    Type getType(int option) {
-        if (find(option, getArgs(STRING)) == 1) {
-            return STRING;
-        } 
-        if (find(option, getArgs(BOOLEAN)) == 1) {
-            return BOOLEAN;
-        } 
-        if (find(option, getArgs(EXISTS)) == 1) {
-            return EXISTS;
-        } 
-
-        //not reached
-        return ERROR;
+        return &((*iter).second);
     }
 
     int isExist(int option) {
-        if (find(option, getArgs(STRING)) == 1) {
-            return 1;
-        } 
-        if (find(option, getArgs(BOOLEAN)) == 1) {
-            return 1;
-        } 
-        if (find(option, getArgs(EXISTS)) == 1) {
-            return 1;
-        } 
-    
-        return 0;
+        return (find(option, getArgs()) != nullptr);
     }
 
-    std::string getOptionString(std::map<int, struct Command> *args, int needParam) {
+    std::string getOptionString(std::map<int, struct Command> *args) {
         std::string option;
+        int needParam = 0;
         for (auto iter = args->begin(); iter != args->end(); iter++) {
             option += (char)((*iter).first); 
+            needParam = getNeedParam(((*iter).second).type);
             if (needParam) {
                 option += ":";
             }
         }
 
         return option;
+    }
+
+    Type getType(int option) {
+        struct Command *command = find(option, getArgs());
+        return (command != nullptr) ? command->type : ERROR;
     }
 
     void parse_opt(const char *options) {
@@ -144,24 +121,24 @@ public:
             argsMap_.insert(std::pair<int, std::string>(opt, value));
         } 
 
-        makeUpForBooleanType();
+        makeUpForExistsType();
     }
 
-    void makeUpForBooleanType() {
-        std::map<int, struct Command> *args = getArgs(EXISTS);
+    void makeUpForExistsType() {
+        std::map<int, struct Command> *args = getArgs();
         for (auto iter = args->begin(); iter != args->end(); iter++) {
             int opt = (*iter).first;
-            if (argsMap_.find(opt) == argsMap_.end()) {
-                argsMap_.insert(std::pair<int, std::string>(opt, "false"));
+            if ((*iter).second.type == EXISTS) {
+                if (argsMap_.find(opt) == argsMap_.end()) {
+                    argsMap_.insert(std::pair<int, std::string>(opt, "false"));
+                }
             }
         }
     }
 
     void parse() {
         std::string options;
-        options += getOptionString(getArgs(STRING), getNeedParam(STRING));
-        options += getOptionString(getArgs(BOOLEAN), getNeedParam(BOOLEAN));
-        options += getOptionString(getArgs(EXISTS), getNeedParam(EXISTS));
+        options += getOptionString(getArgs());
         parse_opt(options.c_str());
     }
 
@@ -170,9 +147,7 @@ public:
     }
 
 private:
-    std::map<int, struct Command> boolean_args_; 
-    std::map<int, struct Command> string_args_; 
-    std::map<int, struct Command> exists_args_; 
+    std::map<int, struct Command> args_; 
     std::map<int, std::string> argsMap_; 
     char **argv_;
     int argc_;
